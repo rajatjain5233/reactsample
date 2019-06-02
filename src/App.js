b473d9d3621e536416974b064bc23baa72cb3f1b
rajatjain5233 <jain.rajat5233@gmail.com>
2019-06-02 19:10:25 +0530

    Sample React Starter

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+class App extends React.Component {
+    
+    
+
+    async componentWillMount(){
+       //console.log("jjjjjjjjjjj");
+    }
+
+    render() {
+        return (
+            <div>
+                 hello
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import './myStyles.scss';
+
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
